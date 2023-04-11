3b7a49c53e4b4a0760ab11a335a47999f499e2a8
Muzzle886 <2568682437@qq.com>
2023-04-11 14:48:22 +0800

    resonsive test

--- src/responsive/responsive.ts
+++ src/responsive/responsive.ts
@@ -1,8 +1,15 @@
+/**
+ * 此版本主要对响应式系统进行测试，在响应式数据obj上设置一个不存在的属性
+ * 我们没有在副作用函数与被操作的目标字段之间建立明确的联系，导致我们没有读取text属性但是副作用函数依旧被执行了，这是不正确的
+ */
+
 // 用一个全局变量存储被注册的副作用函数
 let activeEffect: Function;
 // effect函数用于注册副作用函数
 function effect(fn: Function) {
+  // 当调用effect注册副作用函数时，将副作用函数fn赋值给activeEffect
   activeEffect = fn;
+  // 执行副作用函数
   fn();
 }
 
@@ -33,11 +40,16 @@ const obj = new Proxy(data, {
   },
 });
 
-effect(() => {
-  document.querySelector(".responsive").innerHTML = obj.text;
-});
+effect(
+  // 匿名副作用函数
+  () => {
+    console.log('effect run'); // 会打印两次
+    document.querySelector(".responsive").innerHTML = obj.text;
+  }
+);
 setTimeout(() => {
-  obj.text = "hello vue3";
+  // 副作用函数中并没有读取notExist属性的值
+  obj.notExist = "hello vue3";
 }, 1000);
 
 export default {};
